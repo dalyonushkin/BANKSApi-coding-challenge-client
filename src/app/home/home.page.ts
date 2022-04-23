169603cead37b73c958eba6f193a8ab8c2bf4e93
Denis Alyonushkin <denis.alyonushkin@gmail.com>
2022-04-23 22:05:29 +0300

    feat: add mock transfer list page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  mockData=[
+    {
+      date: new Date(),
+      amount:123.33,
+      accountHolder:'John Doe',
+      iban:'DE12500105170648489890',
+      note:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+scrambled it to make a type specimen book. It has survived not only five centuries, but also theleap
+into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+software like Aldus PageMaker including versions of Lorem Ipsum.`
+    },
+    {      date: new Date('2022-04-01'),
+      amount:99999999.33,
+      accountHolder:'Jane Doe',
+      iban:'DE12500105170648489890',
+      note:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+scrambled it to make a type specimen book. It has survived not only five centuries, but also theleap
+into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+software like Aldus PageMaker including versions of Lorem Ipsum.`},
+{      date: new Date('2022-04-10'),
+  amount:9999999999999.333333,
+  accountHolder:'Baby Doe',
+  iban:'DE12500105170648489890'},
+  {      date: new Date('2022-04-10'),
+  amount:9999999999999.333333,
+  accountHolder:'James Doe',
+  iban:'DE12500105170648489890'},
+  {      date: new Date('2022-04-10'),
+  amount:9999999999999.333333,
+  accountHolder:'Judy Doe',
+  iban:'DE12500105170648489890'}
+  ];
   constructor() {}
 
 }
